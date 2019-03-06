b8f3e65b536012972746b98872159fe2c658d82a
Cody-Price <ctprice11@gmail.com>
2019-03-06 16:50:02 -0700

    Add menu toggle ability

---
+++ public/script.js
@@ -0,0 +1,22 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const main = document.querySelector('.main');
+const header = document.querySelector('.header');
+
+const toggleMenu = () => {
+  if (menu.classList.contains('displayed')) {
+    menu.classList.add('not-displayed')
+    menu.classList.remove('displayed')
+    main.setAttribute("style", "width: 100vw; left: 0px; transition: 0.3s;")
+    header.setAttribute("style", "width: 100vw; left: 0px; transition: 0.3s;")
+  } else {
+    setTimeout(() => {
+      menu.classList.remove('not-displayed')
+      menu.classList.add('displayed')
+    }, 300);
+    main.setAttribute("style", "width: 85vw; left: 15.5vw; transition: 0.3s;")
+    header.setAttribute("style", "width: 85vw; left: 15.5vw; transition: 0.3s;")
+  }
+}
+
+menuBtn.addEventListener('click', toggleMenu)
